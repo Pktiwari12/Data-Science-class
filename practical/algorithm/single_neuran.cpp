a7c41f776ce3ac259a6f614846aadb27d9fde27b
Pktiwari12 <pradeepkumargarautha2005@gmail.com>
2025-09-02 17:05:03 +0530

    Final Algorithm complete for single neuran

--- practical/algorithm/single_neuran.cpp
+++ practical/algorithm/single_neuran.cpp
@@ -7,29 +7,21 @@ vector<float>single_neuran_step_fxn(float lambda, float w0, float w1, float w2, 
     int w_not_changed = false;
     int count = 1;
     while(w_not_changed == false){
+            w_not_changed = true;
             cout << count <<"  Epoch  :" << endl << endl;
             for(int i = 0 ; i < data.size() ; i++){
-                // cout << "For fist instance " << endl << endl << endl;
-                // cout << w0 << "\t" << w1 << "\t" << w2 << endl;
-                // find summation of wixi
                 float b = w0*x0;    // bias term
                 float x1 = data[i][0];
                 float x2 = data[i][1];
                 float input = b + w1*x1 + w2*x2;
-                // cout << input << endl;
                 float y = step_fxn(input); // prediction
-                // cout << y << endl;
                 float d = data[i][2];   // target value
-                // cout << d << endl ;
-                // if prediction is not correct
                 if( y != data[i][2]){
                     // adaptive law
                     w0 = w0 + lambda*(d-y)*x0;
                     w1 = w1 + lambda*(d-y)*x1;
                     w2 = w2 + lambda*(d-y)*x2;
                     w_not_changed = false; // check this flag.
-                }else{
-                    w_not_changed = true;
                 }
 
             }
@@ -56,8 +48,8 @@ int main(){
         {0.5, 0.2, 1},
         {0.7, 0.9, 0}
     };
-    for(int i = 1 ; i< 15 ; i++){
-        single_neuran_step_fxn(i,1.0,1.0,1.0,1,data);
-    }
+    // for(int i = 1 ; i< 15 ; i++){
+        single_neuran_step_fxn(1,0,0,0,1,data);
+    // }
 
 }
